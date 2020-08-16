7b2c1a2b062e9c47909aae2b5ae033d71ad109bf
RAHUL KUMAR <55033230+pctablet505@users.noreply.github.com>
2020-08-16 16:16:01 +0000

    Add files via upload

---
+++ Python/Nested Lists.py
@@ -0,0 +1,8 @@
+marksheet=[]
+for _ in range(0,int(input())):
+    name=input()
+    score=float(input())
+    marksheet.append([name,score])
+
+second_lowest=sorted(list(set([marks for name , marks in marksheet])))[1]
+print('\n'.join([name for name,marks in sorted(marksheet) if marks==second_lowest]))
