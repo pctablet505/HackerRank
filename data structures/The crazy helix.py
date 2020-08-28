725347ffce924524d981903aee1344f85c4dce10
RAHUL KUMAR <55033230+pctablet505@users.noreply.github.com>
2020-08-28 12:57:18 +0530

    this solution works in python3

---
+++ data structures/The crazy helix.py
@@ -0,0 +1,197 @@
+from sys import stdin,stdout
+def size(node):
+    if node:
+        return node.size
+    return 0
+
+
+class Node:
+        
+    def __init__(self, id=-1):
+        self.id = id
+        self.left =None
+        self.right =None
+        self.parent = None
+        self.size = 1
+        self.rev = False
+
+    def release(self):
+        if self.rev:
+            self.rev = False
+            self.left, self.right = self.right, self.left
+            if self.left:
+                self.left.rev ^= True
+            if self.right:
+                self.right.rev ^= True
+
+    def update(self):
+        self.size = 1 + size(self.left) + size(self.right)
+
+
+def zig(p):
+    q = p.parent
+    r = q.parent
+    q.left=p.right
+    if q.left:
+        q.left.parent = q
+    p.right = q
+    q.parent = p
+    p.parent=r
+    
+    if p.parent:
+        if r.left == q:
+            r.left = p
+        if r.right == q:
+            r.right = p
+    q.update()
+
+
+def zag(p):
+    q = p.parent
+    r = q.parent
+    q.right=p.left
+    if q.right:
+        q.right.parent = q
+    p.left = q
+    q.parent = p
+    p.parent=r
+    if p.parent:
+        if r.left == q:
+            r.left = p
+        if r.right == q:
+            r.right = p
+    q.update()
+
+
+def splay(root, p, b=None):
+    p.release()
+    while p.parent != b:        
+        q = p.parent
+        
+        if q.parent == b:
+            q.release()
+            p.release()
+            if q.left == p:
+                zig(p)
+            else:
+                zag(p)
+        else:
+            r = q.parent
+            r.release()
+            q.release()
+            p.release()
+            if r.left == q:
+                if q.left == p:
+                    zig(q)
+                    zig(p)
+                else:
+                    zag(p)
+                    zig(p)
+            elif q.left == p:
+                zig(p)
+                zag(p)
+            else:
+                zag(q)
+                zag(p)
+    p.update()
+    if b == None:
+        root = p
+    else:
+        b.update()
+    return root
+
+
+def find(k, p):
+    if not p or p.size < k:
+        return None
+    while k:
+        p.release()
+        if p.left and p.left.size >= k:
+            p = p.left
+        else:
+            if p.left:
+                k -= p.left.size
+            k -= 1
+            if k > 0:
+                p = p.right
+    return p
+
+
+def build( a, b):
+    
+    global T
+    
+    if a > b:
+        return None
+    if a == b:
+        prx = T[a]
+        prx.left =None
+        prx.right =None 
+        prx.parent = None
+        
+        return prx
+    mid = (a + b)//2
+    prx = T[mid]
+    prx.parent = None
+
+    prx.left = build( a, mid - 1)
+    
+    if prx.left:
+        prx.left.parent = prx
+    prx.right = build( mid + 1, b)
+    if prx.right:
+        prx.right.parent = prx
+    prx.update()
+    
+    
+    return prx
+
+
+def reverse(root, a, b):
+    if a == b:
+        return
+    lfx = a + 1
+    rgx = b + 1
+    prev = find(lfx - 1, root)
+    nxt = find(rgx + 1, root)
+    root=splay(root, prev)
+    root=splay(root, nxt, prev)
+    nxt.left.rev ^= True
+    return root
+
+
+
+
+
+n, q = map(int, stdin.readline().strip().split())
+queries=[]
+T = [None for i in range(n + 2)]
+for i in range(n + 2):
+    T[i] = Node(i)
+
+
+root = build( 0, n + 1)
+
+s = ''
+for k in range(q):
+    queries.append(tuple(map(int, stdin.readline().strip().split())))
+for query in queries:
+    t = query[0]
+    
+    if t == 1:
+        i, j = query[1], query[2]
+        root=reverse(root, i, j)
+    
+    elif t == 2:
+        i = query[1]
+        ptx = T[i]
+        root = splay(root, ptx)
+        s += 'element {} is at position {}\n'.format(i, size(ptx.left))
+    
+    else:
+        i = query[1]
+        ptx = find(i + 1,root)
+        s += 'element at position {} is {}\n'.format(i, ptx.id)
+stdout.write(s)
+
+
--- data structures/The crazy helix.pyi
+++ data structures/The crazy helix.pyi
@@ -2,166 +2,200 @@
 '''The same algorithm works in java and c++'''
 '''please see java code'''
 
-
-import sys
-from sys import stdin, stdout
-
-sys.setrecursionlimit(99999999)
-from random import randint
-
-
-def rand():
-    return randint(1, 2 ** 64)
-
-
-class Node:
-    def __init__(self, key, priority, size=1):
-        self.priority = priority
-        self.key = key
-        self.left = None
-        self.right = None
-        self.size = size
-        self.rev = False
-
-
-def size(item):
-    if item:
-        return item.size
+from sys import stdin,stdout
+def size(node):
+    if node:
+        return node.size
     return 0
 
 
-def update_count(node):
-    if node:
-        node.size = size(node.left) + size(node.right) + 1
+class Node:
+        
+    def __init__(self, id=-1):
+        self.id = id
+        self.left =None
+        self.right =None
+        self.parent = None
+        self.size = 1
+        self.rev = False
+
+    def release(self):
+        if self.rev:
+            self.rev = False
+            self.left, self.right = self.right, self.left
+            if self.left:
+                self.left.rev ^= True
+            if self.right:
+                self.right.rev ^= True
+
+    def update(self):
+        self.size = 1 + size(self.left) + size(self.right)
 
 
-def push(node):
-    if node and node.rev:
-        node.rev = False
-        node.left, node.right = node.right, node.left
-        if node.left:
-            node.left.rev ^= True
-        if node.right:
-            node.right.rev ^= True
+def zig(p):
+    q = p.parent
+    r = q.parent
+    q.left=p.right
+    if q.left:
+        q.left.parent = q
+    p.right = q
+    q.parent = p
+    p.parent=r
+    
+    if p.parent:
+        if r.left == q:
+            r.left = p
+        if r.right == q:
+            r.right = p
+    q.update()
 
 
-def merge(l, r):
-    push(l)
-    push(r)
-    if not l:
-        return r
-    if not r:
-        return l
-    elif l.priority > r.priority:
-        l.right = merge(l.right, r)
-        t = l
+def zag(p):
+    q = p.parent
+    r = q.parent
+    q.right=p.left
+    if q.right:
+        q.right.parent = q
+    p.left = q
+    q.parent = p
+    p.parent=r
+    if p.parent:
+        if r.left == q:
+            r.left = p
+        if r.right == q:
+            r.right = p
+    q.update()
+
+
+def splay(root, p, b=None):
+    p.release()
+    while p.parent != b:        
+        q = p.parent
+        
+        if q.parent == b:
+            q.release()
+            p.release()
+            if q.left == p:
+                zig(p)
+            else:
+                zag(p)
+        else:
+            r = q.parent
+            r.release()
+            q.release()
+            p.release()
+            if r.left == q:
+                if q.left == p:
+                    zig(q)
+                    zig(p)
+                else:
+                    zag(p)
+                    zig(p)
+            elif q.left == p:
+                zig(p)
+                zag(p)
+            else:
+                zag(q)
+                zag(p)
+    p.update()
+    if b == None:
+        root = p
     else:
-        r.left = merge(l, r.left)
-        t = r
-    update_count(t)
-    return t
+        b.update()
+    return root
 
 
-def getValue(t, key, add=0):
-    push(t)
-    cur_key = add + size(t.left)
-    if key == cur_key:
-        return t.key
-    if key < cur_key:
-        return getValue(t.left, key, add)
-    else:
-        return getValue(t.right, key, add + 1 + size(t.left))
+def find(k, p):
+    if not p or p.size < k:
+        return None
+    while k:
+        p.release()
+        if p.left and p.left.size >= k:
+            p = p.left
+        else:
+            if p.left:
+                k -= p.left.size
+            k -= 1
+            if k > 0:
+                p = p.right
+    return p
 
 
-def split(t, key, add=0):
-    if not t:
-        return (None, None)
-    push(t)
-    curr_key = add + size(t.left)
-    if key <= curr_key:
-        l, t.left = split(t.left, key, add)
-        r = t
-    else:
-        t.right, r = split(t.right, key, add + 1 + size(t.left))
-        l = t
-    update_count(t)
-    return (l, r)
+def build( a, b):
+    
+    global T
+    
+    if a > b:
+        return None
+    if a == b:
+        prx = T[a]
+        prx.left =None
+        prx.right =None 
+        prx.parent = None
+        
+        return prx
+    mid = (a + b)//2
+    prx = T[mid]
+    prx.parent = None
+
+    prx.left = build( a, mid - 1)
+    
+    if prx.left:
+        prx.left.parent = prx
+    prx.right = build( mid + 1, b)
+    if prx.right:
+        prx.right.parent = prx
+    prx.update()
+    
+    
+    return prx
 
 
-def reverse(t, l, r):
-    t1, t2 = split(t, l)
-    t2, t3 = split(t2, r - l + 1)
-    t2.rev ^= True
-    t = merge(t1, t2)
-    t = merge(t, t3)
-    return t
-
-
-def insert(t, index, value):
-    t1, t2 = split(t, index)
-    t = merge(t1, Node(value, rand()))
-    t = merge(t, t2)
-    return t
-
-
-def kth(root, k):
-    push(root)
-    if k < size(root.left):
-        return kth(root.left, k)
-    elif k > size(root.left):
-        return kth(root.right, k - size(root.left) - 1)
-    return root.key
-
-
-def traverse(root):
-    global pos
-    global snapshot
-    if not root:
+def reverse(root, a, b):
+    if a == b:
         return
-    push(root)
-    traverse(root.left)
-    snapshot[pos] = root.key
-    pos += 1
-    traverse(root.right)
+    lfx = a + 1
+    rgx = b + 1
+    prev = find(lfx - 1, root)
+    nxt = find(rgx + 1, root)
+    root=splay(root, prev)
+    root=splay(root, nxt, prev)
+    nxt.left.rev ^= True
+    return root
 
 
-n, q = map(int, stdin.readline().split())
-root = None
-snapshot = [0] * n
-inv = [0] * n
-for i in range(n):
-    root = insert(root, i, i)
-    inv[i] = i
-M = 300
-left = [0] * M
-right = [0] * M
-count = 0
+
+
+
+n, q = map(int, stdin.readline().strip().split())
+queries=[]
+T = [None for i in range(n + 2)]
+for i in range(n + 2):
+    T[i] = Node(i)
+
+
+root = build( 0, n + 1)
+
 s = ''
-for i in range(q):
-    query = list(map(int, stdin.readline().split()))
-    type = query[0]
-    if type == 1:
-        a = query[1] - 1
-        b = query[2] - 1
-        root = reverse(root, a, b)
-        left[count] = a
-        right[count] = b
-        count += 1
-        if count == M:
-            pos = 0
-            traverse(root)
-            for j in range(n):
-                inv[snapshot[j]] = j
-            count = 0
-    elif type == 2:
-        x = query[1] - 1
-        cur = inv[x]
-        for j in range(count):
-            if left[j] <= cur and cur <= right[j]:
-                cur = left[j] + right[j] - cur
-
-        s += 'element {} is at position {}\n'.format(x + 1, cur + 1)
+for k in range(q):
+    queries.append(tuple(map(int, stdin.readline().strip().split())))
+for query in queries:
+    t = query[0]
+    
+    if t == 1:
+        i, j = query[1], query[2]
+        root=reverse(root, i, j)
+    
+    elif t == 2:
+        i = query[1]
+        ptx = T[i]
+        root = splay(root, ptx)
+        s += 'element {} is at position {}\n'.format(i, size(ptx.left))
+    
     else:
-        y = query[1] - 1
-        s += 'element at position {} is {}\n'.format(y + 1, kth(root, y) + 1)
+        i = query[1]
+        ptx = find(i + 1,root)
+        s += 'element at position {} is {}\n'.format(i, ptx.id)
 stdout.write(s)
+
+
